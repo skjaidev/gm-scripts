3dc67dea3a8d6f63cbd791ce5c275e67d5d3d42b
Jaidev Sridhar <mail@jaidev.info>
2005-12-02 22:25:45 +0000

    Adds BCC to the right message in a conversation view!

--- gmailAutoBcc.user.js
+++ gmailAutoBcc.user.js
@@ -1,4 +1,4 @@
-/* This greasemonkey script automatically BCCs outgoing email to a 
+/* This greasemonkey script automatically BCCs outgoing email from a gmail address to a 
  *  specified email address
  * 
  *  $Id$
@@ -12,37 +12,34 @@
 // ==UserScript==
 // @name          Gmail Auto BCC
 // @namespace     http://jaidev.info/home/projects/gmailAutoBcc
-// @description   This greasemonkey script automatically BCCs outgoing email to a specified email address
+// @description   This greasemonkey script automatically BCCs outgoing email from a gmail address to a specified email address
 // @include       http*://mail.google.com/mail/*
 // ==/UserScript==
-if (!GM_getValue('gBccMail')) {
-	email = prompt("gmailAutoBcc: Where do you want to bcc all your outgoing gmail?");
-	GM_setValue('gBccMail', email);
-} 
 document.addEventListener ('click', function(event) {
 	if (event.target.getAttribute ("id") == "send") {
+		var form, div;
 		var email = GM_getValue('gBccMail');
-		var bcc = document.getElementById ("bcc_compose");
-		if (bcc) {
-			var bccs = bcc.value;
-			if (bccs) {
-				/* TBD: Don't do it if it is already done!  */
-				bcc.value = bccs+"," +email;
-			}
-			else 
-				bcc.value = email;
-			return;
+		if (!email) {
+			email = prompt("gmailAutoBcc: Where do you want to bcc all your outgoing gmail?");
+			GM_setValue('gBccMail', email);
 		}
-		var bcc = document.getElementById ("bcc_0");
-		if (bcc) {
-			var bccs = bcc.value;
+		var msg_type = event.target.form.parentNode.getAttribute("id");
+		var bcc_id;
+		if (msg_type == "cm_compose") {
+			bcc_id = "bcc_compose";
+		}
+		else {
+			bcc_id = "bcc_"+ msg_type.substr (3,1);
+		}
+		var bcc_field = document.getElementById (bcc_id);
+		if (bcc_field) { 
+			var bccs = bcc_field.value;
 			if (bccs) {
 				/* TBD: Don't do it if it is already done!  */
-				bcc.value = bccs+", "+email;
+				bcc_field.value = bccs+"," +email;
 			}
 			else 
-				bcc.value = email;
-			return;
+				bcc_field.value = email;
 		}
 	}
 	}, 
