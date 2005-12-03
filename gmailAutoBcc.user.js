069ec1b9812b6f9e39db4e2236b46fc156f2add9
Jaidev Sridhar <mail@jaidev.info>
2005-12-03 02:20:29 +0000

    Added tweaks!

--- gmailAutoBcc.user.js
+++ gmailAutoBcc.user.js
@@ -16,13 +16,35 @@
 // @description   This greasemonkey script automatically BCCs outgoing email from a gmail address to a specified email address
 // @include       http*://mail.google.com/mail/*
 // ==/UserScript==
+
+// gBccMail = email Address		Email address to BCC to
+// gBccEnabled = true / false		
+// gBccPopup = true / false	Pops up a prompt before adding BCC
+
 document.addEventListener ('click', function(event) {
 	if (event.target.getAttribute ("id") == "send") {
-		var form, div;
+		enabled = GM_getValue('gBccEnabled');
+		if (enabled == false) {
+			return;
+		}
+		else if (enabled != true) {
+			GM_setValue('gBccEnabled', true);
+			GM_setValue('gBccPopup', false); // FALSE by default
+			enabled = true;
+
+		}
 		var email = GM_getValue('gBccMail');
 		if (!email) {
 			email = prompt("gmailAutoBcc: Where do you want to bcc all your outgoing gmail?");
 			GM_setValue('gBccMail', email);
+		}
+		var popup = GM_getValue ('gBccPopup');
+		if (popup == true) {
+			if (!confirm("Do you want to add BCC to " + email + "?"))
+				return;
+		}
+		else if (popup != false) {
+			GM_setValue ('gBccPopup', false); // FALSE by default
 		}
 		var msg_type = event.target.form.parentNode.getAttribute("id");
 		var bcc_id;
