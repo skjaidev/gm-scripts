603f4c410f9cd36ff09958722b1108de1cbee2ee
Jaidev Sridhar <mail@jaidev.info>
2005-12-05 08:28:55 +0000

    fixed issue with spaces in labels

--- gmailUnlabelled.user.js
+++ gmailUnlabelled.user.js
@@ -19,7 +19,7 @@ var edit_labels = document.getElementById ('prf_l');
 if (edit_labels) {
 	var label_none = edit_labels.cloneNode (true);
 	label_none.setAttribute ("id", "label_none");
-	label_none.setAttribute ("onclick", "var iter = this; var str = ''; while (iter.previousSibling != null) { iter = iter.previousSibling; str = str + ' -label:' + iter.getAttribute('id').substr(3); }var s = document.getElementById ('s'); for (var i = 0; i < s.childNodes.length; i++) {	var q = s.childNodes[i]; 	if (q.getAttribute('name') == 'q') 	{		q.value = str; 		return top.js._MH_OnSearch(window,0);	}}");
+	label_none.setAttribute ("onclick", "var iter = this; var str = ''; while (iter.previousSibling != null) { iter = iter.previousSibling; str = str + ' -label:' + iter.getAttribute('id').substr(3).replace(' ', '_'); }var s = document.getElementById ('s'); for (var i = 0; i < s.childNodes.length; i++) {	var q = s.childNodes[i]; 	if (q.getAttribute('name') == 'q') 	{		q.value = str; 		return top.js._MH_OnSearch(window,0);	}}");
 	label_none.innerHTML = "Unlabelled";
 	edit_labels.parentNode.insertBefore (label_none, edit_labels);
 }
