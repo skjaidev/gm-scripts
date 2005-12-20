a4e33317f548e0c56bfb3f50b46b081a24f93087
Jaidev Sridhar <mail@jaidev.info>
2005-12-20 01:25:36 +0000

    Deals with gmails prefetch now

--- gmailUnlabelled.user.js
+++ gmailUnlabelled.user.js
@@ -15,11 +15,29 @@
 // @include       http*://mail.google.com/*
 // ==/UserScript==
 
-var edit_labels = document.getElementById ('prf_l');
-if (edit_labels) {
-	var label_none = edit_labels.cloneNode (true);
-	label_none.setAttribute ("id", "label_none");
-	label_none.setAttribute ("onclick", "var iter = this; var str = ''; while (iter.previousSibling != null) { iter = iter.previousSibling; str = str + ' -label:' + iter.getAttribute('id').substr(3).replace(' ', '-'); }var s = document.getElementById ('s'); s.elements.namedItem('q').value = str; return top.js._MH_OnSearch(window,0);");
-	label_none.innerHTML = "Unlabelled";
-	edit_labels.parentNode.insertBefore (label_none, edit_labels);
+
+
+function process_frame (id)
+{
+	var iframe = document.getElementById(id);
+	if (iframe) {
+		iframe.addEventListener ('DOMAttrModified', function () {
+			window.setTimeout (function () {
+				if (!iframe.wrappedJSObject.contentDocument.getElementById ('label_none')) {
+					var edit_labels = iframe.wrappedJSObject.contentDocument.getElementById ('prf_l');
+					if (edit_labels) {
+						var label_none = edit_labels.cloneNode (true);
+						label_none.setAttribute ("id", "label_none");
+						label_none.setAttribute ("onclick", "var iter = this; var str = ''; while (iter.previousSibling != null) { iter = iter.previousSibling; str = str + ' -label:' + iter.getAttribute('id').substr(3).replace(' ', '-'); }var s = document.getElementById ('s'); s.elements.namedItem('q').value = str; return top.js._MH_OnSearch(window,0);");
+						label_none.innerHTML = "Unlabelled";
+						edit_labels.parentNode.insertBefore (label_none, edit_labels);
+					}
+				}
+			}, 0);
+		}, true);
+	}
 }
+
+process_frame ('v1');
+process_frame ('v2');
+
