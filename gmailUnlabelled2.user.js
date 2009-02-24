2fc463688e0535e0683c3fd3d45f04f4112440bc
Jaidev Sridhar <sridharj@t13.nowlab.cis.ohio-state.edu>
2009-02-24 18:27:52 -0500

    Update for new gmail layout + fix for delayed gmail API loads.

--- gmailUnlabelled2.user.js
+++ gmailUnlabelled2.user.js
@@ -9,66 +9,93 @@
  */
 
 // ==UserScript==
-// @name          Gmail Unlabelled 2.0
-// @namespace     http://jaidev.info/home/hacks/gmailUnlabelled
+// @name      Gmail Unlabelled 2.0
+// @namespace   http://jaidev.info/home/hacks/gmailUnlabelled
 // @description   This script adds 'Unlabelled' at the end of the labels list to search for unlabelled conversations. This version is for the "new" version of gmail (Nov 2007).
-// @include       http*://mail.google.com/*
+// @include     http*://mail.google.com/*
 // ==/UserScript==
-
-
-var LABELS_TABLE_DIV_CLASS = "pvSW6e";
-var SEARCH_DIV_CLASS = "tYBwhe";
-
-window.addEventListener('load', function() {
-    if (unsafeWindow.gmonkey) {
-        unsafeWindow.gmonkey.load("1.0", function(gmail) {
-            function gmailUnlabelled () {
-               var root = gmail.getNavPaneElement().ownerDocument;
-               if (!root.getElementById("label_none")) {
-                    var expression = ".//div[contains(concat(' ', @class, ' '), ' " + LABELS_TABLE_DIV_CLASS + " ')]";
-                    var table_div= root.evaluate(expression, root, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null).iterateNext();
-                    table_div.setAttribute ("id", "label_table_div");
-                    var label_none = table_div.nextSibling.cloneNode (true);
-                    label_none.setAttribute ("id", "label_none");
-                    label_none.innerHTML = "Unlabelled";
-                    label_none.addEventListener ("click", function(event) {
-                        event.cancelBubble = true;
-                        var table_div = this.ownerDocument.getElementById("label_table_div");
-                        var tr = table_div.firstChild.firstChild.firstChild;
-                        var QUERY_STR = "";
-                        while (tr) {
-                            var label = tr.firstChild.firstChild.firstChild.firstChild.innerHTML;
-                            QUERY_STR = QUERY_STR + ' -label:' + label.replace(/[/\ &]/g, '-');
-                            tr = tr.nextSibling;
-                        }
-                        var search_div = this.ownerDocument.getElementById("search_div");
-                        search_div.parentNode.previousSibling.firstChild.value = QUERY_STR;
-                        search_div.firstChild.click ();
-                        }, true);
-                    table_div.parentNode.insertBefore(label_none, table_div.nextSibling);
-                    expression = ".//div[contains(concat(' ', @class, ' '), ' " + SEARCH_DIV_CLASS + " ')]";
-                    var search_div = root.evaluate(expression, root, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null).iterateNext();
-                    search_div.setAttribute ("id", "search_div");
-                    search_div.parentNode.previousSibling.firstChild.addEventListener ("change", function (event) {
-                        if ("-label" == this.value) { 
-                            var table_div = this.ownerDocument.getElementById("label_table_div");
-                            var tr = table_div.firstChild.firstChild.firstChild;
-                            var QUERY_STR = "";
-                            while (tr) {
-                                var label = tr.firstChild.firstChild.firstChild.firstChild.innerHTML;
-                                QUERY_STR = QUERY_STR + ' -label:' + label.replace(/[/\ &]/g, '-');
-                                tr = tr.nextSibling;
-                            }
-                            this.value=QUERY_STR;
-                            //this.nextSibling.click ();
-                        }
-                    }, true);
-                }
+var LDC = "pvSW6e";
+var SIC = "ZRiJh mFwySd";
+var SID = ":ra";
+window.addEventListener ('load', function () {
+  if (unsafeWindow.gmonkey) {
+    unsafeWindow.gmonkey.load ("1.0", function (gmail) {
+      function gmailUnlabelled () {
+        var root = gmail.getNavPaneElement ().ownerDocument;
+        if (!root.getElementById ("label_none")) {
+          var expr = ".//div[contains (concat (' ', @class, ' '), ' " + LDC 
+              + " ')]";
+          var tab_div = root.evaluate (expr, root, null, 
+              XPathResult.ORDERED_NODE_ITERATOR_TYPE, null).iterateNext ();
+          tab_div.setAttribute ("id", "lb_tbl_div");
+          var label_none = tab_div.nextSibling.cloneNode (true);
+          label_none.setAttribute ("id", "label_none");
+          label_none.innerHTML = "Unlabelled";
+          label_none.addEventListener ("click", function (event) {
+            event.cancelBubble = true;
+            var tab_div = this.ownerDocument.getElementById ("lb_tbl_div");
+            var tr = tab_div.firstChild.firstChild.firstChild;
+            var QS = "";
+            var l = "";
+            while (tr) {
+              l = tr.firstChild.firstChild.firstChild.firstChild.innerHTML;
+              QS = QS + ' -label:' + l.replace (/[/\ &]/g, '-');
+              tr = tr.nextSibling;
             }
-            gmailUnlabelled ();
-            gmail.registerViewChangeCallback (gmailUnlabelled);
-        });
-    }
+            var srch_ip = this.ownerDocument.getElementById (SID);
+            srch_ip.value = QS;
+            srch_ip.focus ();
+            var evt = srch_ip.ownerDocument.createEvent ("KeyboardEvent");
+            evt.initKeyEvent ("keypress", true, false, null,
+                false, false, false, false, 0x0D, 0);
+            srch_ip.dispatchEvent (evt);
+            }, true);
+          tab_div.parentNode.insertBefore (label_none, tab_div.nextSibling);
+          var srch_ip = root.getElementById (SID);
+          srch_ip.addEventListener ("keypress", function (event) {
+            if (event.keyCode == 0x0D && event.charCode == 0 && 
+                "-label" == this.value) { 
+              var tab_div = this.ownerDocument.getElementById ("lb_tbl_div");
+              var tr = tab_div.firstChild.firstChild.firstChild;
+              var QS = "";
+              var l = "";
+              while (tr) {
+                l = tr.firstChild.firstChild.firstChild.firstChild.innerHTML;
+                QS = QS + ' -label:' + l.replace (/[/\ &]/g, '-');
+                tr = tr.nextSibling;
+              }
+              this.value = QS;
+              this.focus ();
+              var evt = this.ownerDocument.createEvent ("KeyboardEvent");
+              evt.initKeyEvent ("keypress", true, false, null,
+                  false, false, false, false, 0x0D, 0);
+              this.dispatchEvent (evt);
+            }
+          }, true);
+          srch_ip.addEventListener ("change", function (event) {
+            if ("-label" == this.value) { 
+              var tab_div = this.ownerDocument.getElementById ("lb_tbl_div");
+              var tr = tab_div.firstChild.firstChild.firstChild;
+              var QS = "";
+              var l = "";
+              while (tr) {
+                l = tr.firstChild.firstChild.firstChild.firstChild.innerHTML;
+                QS = QS + ' -label:' + l.replace (/[/\ &]/g, '-');
+                tr = tr.nextSibling;
+              }
+              this.value = QS;
+              var evt = this.ownerDocument.createEvent ("KeyboardEvent");
+              evt.initKeyEvent ("keypress", true, false, null,
+                  false, false, false, false, 0x0D, 0);
+              this.dispatchEvent (evt);
+            }
+          }, true);
+        }
+      }
+      window.setTimeout (gmailUnlabelled, 500);
+      gmail.registerViewChangeCallback (gmailUnlabelled);
+    });
+  }
 }, true);
 
 
