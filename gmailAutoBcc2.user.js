75937369a25aff5de99ab6147095e2ab79f82c6c
Jaidev <jaidev@shire.symonds.net>
2009-12-15 17:56:30 -0800

    Update for gmail dom changes, re-added support for multiple sender identities

--- gmailAutoBcc2.user.js
+++ gmailAutoBcc2.user.js
@@ -25,11 +25,23 @@
 //                                     identities or different gmail accounts
 
 var TOCLS = "dK nr";
-var REBTN = "J-K-I J-J5-Ji J-K-I-Js-KK GZ ipG21e";
-function addBcc(tgt) {
-  var tg_cl = tgt.getAttribute ("class");
+var REBTN = "J-K-I J-J5-Ji J-K-I-Js-KK GZ L3";
+function addBcc (tgt) {
+  var enabled = GM_getValue('gBccEnabled');
+  if (enabled == false) {
+    return;
+  }
+  else if (enabled != true) {
+    /* We're probably running for the first time */
+    GM_setValue('gBccEnabled', true);
+    GM_setValue('gBccPopup', false); // FALSE by default
+    GM_setValue('gBccMapFromAddress', false); // FALSE by default
+    enabled = true;
+  }
+//  var tg_cl = tgt.getAttribute ("class");
   var form;
-  if (tg_cl == TOCLS) {
+// if (tg_cl == TOCLS) {
+  if (tgt.form) {
     form = tgt.form;
   }
   else {
@@ -45,13 +57,24 @@ function addBcc(tgt) {
     dst_field = form.elements.namedItem('cc');
   else 
     dst_field = form.elements.namedItem('bcc');
-  if (dst_field.getAttribute ("gid") == "gBccDone")
-      return;
   /* Get the address to cc/bcc to */
   var mapFrom = GM_getValue ('gBccMapFromAddress');
+  var remove = false;
   if (mapFrom == true) {
     var from = form.elements.namedItem('from').value;
     var email = GM_getValue ('gBccMail_' + from);
+    if (dst_field.getAttribute ("gid") == "gBccDone") {
+      if (tgt.nodeName == 'SELECT') {
+        var lue = GM_getValue ('gBccLU');
+        if (lue == email)
+          return;
+        var lu = new RegExp (lue + "(, )?");
+        remove = true;
+      }
+      else {
+        return;
+      }
+    }
     if (email == "disabled")
       return;
     if (!email) {
@@ -64,6 +87,9 @@ function addBcc(tgt) {
     }
   }
   else {
+    if (dst_field.getAttribute ("gid") == "gBccDone")
+      /* Don't insert again! */
+      return;
     var email = GM_getValue('gBccMail');
     if (!email) {
       email = prompt("gmailAutoBcc: Where do you want to bcc/cc all your outgoing gmail?");
@@ -86,12 +112,24 @@ function addBcc(tgt) {
     GM_setValue ('gBccPopup', false); // FALSE by default
   }
   if (dst_field.value) {
+    if (remove) {
+      var bcc_str = dst_field.value;
+      if (bcc_str.match (lu)) {
+        /* Remove old email */
+        var new_bcc_str = bcc_str.replace (lu, "");
+        var end = new RegExp ("(, )?$");
+        dst_field.value = new_bcc_str.replace (end, "");
+      }
+    }
+  }
+  if (dst_field.value) {
     dst_field.value = dst_field.value+", " +email;
   }
   else {
     dst_field.value = email;
   }
   /* Don't repeat */
+  GM_setValue ('gBccLU', email);
   dst_field.setAttribute("gid", "gBccDone");
 }
 
@@ -101,27 +139,23 @@ if (unsafeWindow.gmonkey) {
     function gBccInit () {
       var root = gmail.getActiveViewElement().ownerDocument;
       root.addEventListener ("blur", function(event) {
-        var enabled = GM_getValue('gBccEnabled');
-        if (enabled == false) {
-          return;
+        if (typeof (event.target.getAttribute) == 'function') {
+          var tg_cl = event.target.getAttribute ("class");
+          if (tg_cl == TOCLS) {
+            addBcc (event.target);
+          }
+          else if (tg_cl == REBTN) {
+            window.setTimeout (addBcc, 500, event.target);
+          }
+          else {
+            return;
+          }
         }
-        else if (enabled != true) {
-          /* We're probably running for the first time */
-          GM_setValue('gBccEnabled', true);
-          GM_setValue('gBccPopup', false); // FALSE by default
-          GM_setValue('gBccMapFromAddress', false); // FALSE by default
-          enabled = true;
+      }, true);
+      root.addEventListener ("change", function (event) {
+        if (event.target.getAttribute ('name') == 'from') {
+          addBcc (event.target);
         }
-        var tg_cl = event.target.getAttribute ("class");
-	if (tg_cl == TOCLS) {
-	  addBcc (event.target);
-	}
-	else if (tg_cl == REBTN) {
-	  window.setTimeout (addBcc, 500, event.target);
-	}
-	else {
-	  return;
-	}
       }, true);
     } /* gBccInit */
     window.setTimeout (gBccInit, 500);
